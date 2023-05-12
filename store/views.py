2360913ca8afe1597e57bebfe5af7554c4132e32
Schleidens <alectineschleidens2016@gmail.com>
2023-05-12 16:02:59 -0400

    create view and template for homepage and add products

--- store/views.py
+++ store/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from django.views.generic import View
+
+from .models import (
+    Product
+)
+
+
 # Create your views here.
+
+class home_page(View):
+    model = Product
+    template = 'home_page.html'
+    
+    def get(self, request):
+        product = self.model.objects.all()
+        
+        return render(request, self.template, {'products': product})
+    
+    def post(self, request):
+        pass
+
+
