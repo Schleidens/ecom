db755551b6372fe48c5e2fd3f91dee3180b3445d
Schleidens <alectineschleidens2016@gmail.com>
2023-05-13 14:23:02 -0400

    set dynamic url for category page

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from .views import (
-    home_page
+    home_page,
+    category_page
 )
 
 
 urlpatterns = [
     path('', home_page.as_view(), name='home-page'),
+    path('<str:slug>', category_page.as_view(), name='category')
 ]
