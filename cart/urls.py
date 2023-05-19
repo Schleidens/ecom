e1e665d4152b088b9fea1eb30195a70d7c13c80f
Schleidens <alectineschleidens2016@gmail.com>
2023-05-19 15:25:23 -0400

    set url for payment handling action with stripe form

--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import add_to_cart, cart_view, remove_from_cart
+from .views import add_to_cart, cart_view, remove_from_cart, handle_payment
 
 
 urlpatterns = [
     path('cart/', cart_view.as_view(), name='cart'),
     path('add-to-cart/<int:product_id>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<int:cart_item_id>/', remove_from_cart, name='remove-from-cart')
+    path('remove-from-cart/<int:cart_item_id>/', remove_from_cart, name='remove-from-cart'),
+    path('handle-payment/', handle_payment, name='handle-payment')
 ]
