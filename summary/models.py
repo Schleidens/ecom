e6d149e79f3fe4810132cc2d457522c248ea7bea
Schleidens <alectineschleidens2016@gmail.com>
2023-05-21 14:00:29 -0400

    create summary model and make migrations

--- summary/models.py
+++ summary/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Summary(models.Model):
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    amount = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.customer.username.capitalize()} {self.amount}$ purchase "
+    
+    
