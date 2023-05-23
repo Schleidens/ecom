f3bacca7ab3b8b096bd8560cb705fd2c6d6c65f6
Schleidens <alectineschleidens2016@gmail.com>
2023-05-23 15:06:56 -0400

    set url for user_login_view

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import user_creation_view
+from .views import user_creation_view, user_login_view
 
 urlpatterns = [
     path('register/', user_creation_view.as_view(), name='register'),
+    path('login/', user_login_view.as_view(), name="login")
 ]
