8efc05746890ea5a598310909fd8c0c59ebe801b
Schleidens <alectineschleidens2016@gmail.com>
2023-05-23 15:06:09 -0400

    set view for login user and render in template

--- authentication/views.py
+++ authentication/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 
 from django.views.generic import View
 
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 
-from .forms import createUserForm
+from .forms import createUserForm, loginUserForm
 
 
 # Create your views here.
@@ -30,3 +30,44 @@ class user_creation_view(View):
             
             return redirect('home-page')
         return render(request, self.template, {'form': form})
+    
+    
+#view for user login with loginUserForm, CBVs
+class user_login_view(View):
+    login_form = loginUserForm
+    template = 'user_login_view.html'
+    message = ''
+    
+    def get(self, request):
+        
+        if request.user.is_authenticated:
+            return redirect('home-page')
+        else:
+            form = self.login_form()   
+            
+            return render(request, self.template, {'form': form})
+    
+    def post(self, request):
+        form = self.login_form(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            user = authenticate(
+                username = username,
+                password = password
+            )
+            
+            if user is not None:
+                login(request, user)
+                
+                return redirect('home-page')
+            
+        message = 'Username or Password incorrect'
+            
+        context = {
+            'form': form,
+            'message' : message
+        }
+        return render(request, self.template, context=context)
