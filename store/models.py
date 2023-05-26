b6e61ef3009df7d2d108c5191b28d287491ea76a
Schleidens <alectineschleidens2016@gmail.com>
2023-05-26 14:30:43 -0400

    add image compression feature while saving any new product with a an image

--- store/models.py
+++ store/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.conf import settings
 
+from PIL import Image
+from io import BytesIO
+from django.core.files import File
+
 from django.utils.text import slugify
 
 # Create your models here.
@@ -16,7 +20,39 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+    def image_compression(self, *args, **kwargs):
+       if self.image:
+            # Open the image using Pillow
+            image = Image.open(self.image)
+            
+            #Resize the image to a maximum size of 1024 x 1024 pixels
+            image.thumbnail((1024, 1024))
+            
+            # Compress the image
+            if self.image.name.lower().endswith('.jpg') or self.image.name.lower().endswith('.jpeg'):
+                format = 'JPEG'
+                # Set the JPEG quality level to 80%
+            elif self.image.name.lower().endswith('.png'):
+                format = 'PNG'
+                # Set the PNG compression level to 6 (out of 9)
+                image = image.convert('P', palette=Image.ADAPTIVE, colors=256)
+                options = {'compress_level': 6}
+            else:
+                # Unsupported image format
+                super(Product, self).save(*args, **kwargs)
+                return
+            
+            output = BytesIO()
+            image.save(output, format=format, optimize=True, quality=80, **options if format == 'PNG' else {})
+            new_image = File(output, name=self.image.name)
+
+            # Set the image field to the compressed image
+            self.image = new_image
+
+            super(Product, self).save(*args, **kwargs)
+    
     def save(self, *args, **kwargs):
+        self.image_compression()
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
     
